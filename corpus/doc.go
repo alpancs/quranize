64fe70abc798765f23e316a015f08a5b3db1b029
Alfan Nur Fauzan <alpancs@gmail.com>
2018-05-28 10:24:20 +0700

    update corpus doc

--- corpus/doc.go
+++ corpus/doc.go
@@ -1,3 +1,4 @@
+// Package corpus provides corpus in go string.
 // Original source of Alquran is taken from http://tanzil.net in XML format.
 //
 // See http://tanzil.net/download/ and http://tanzil.net/trans/.
